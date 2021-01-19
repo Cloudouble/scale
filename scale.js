c4712dea4389a85b0743eaa197ba6de395b2563a
David Pitman <david.pitman@cloudouble.com>
2021-01-19 03:03:36 +0000

    client window scoped code ready for testing

--- scale.js
+++ scale.js
@@ -1,16 +1,50 @@
 window.LiveElement = window.LiveElement || {}
 window.LiveElement.Scale = window.LiveElement.Scale || Object.defineProperties({}, {
     version: {configurable: false, enumerable: true, writable: false, value: '1.0.0'}, 
-    Options: {configurable: false, enumerable: true, writable: true, value: {
+    Options: {configurable: false, enumerable: true, writable: false, value: {
         Listen: true
     }}, 
-    Queue: {configurable: false, enumerable: true, false: true, value: []}
+    Channels: {configurable: false, enumerable: true, writable: false, value: {}}, 
+    OpenChannels: {configurable: false, enumerable: true, writable: false, value: {}}, 
+    isValidMessage: {configurable: false, enumerable: true, writable: false, value: function(message) {
+        return message && typeof message == 'object' && message.meta && typeof message.meta == 'object' && message.payload && typeof message.payload == 'object' && message.meta.channel && message.meta.source
+    }}, 
+    getChannelProcessor: {configurable: false, enumerable: true, writable: false, value: function(channelName, processorName=undefined, subscriptionCallback=undefined, triggerCallback=undefined) {
+        processorName = processorName || channelName
+        if (processorName) {
+            var hasSubscriptionCallback = subscriptionCallback && typeof subscriptionCallback == 'function'
+            var hasTriggerCallback = triggerCallback && typeof triggerCallback == 'function' 
+            if (hasSubscriptionCallback || hasTriggerCallback) {
+                return function(input) {
+                    switch (window.LiveElement.Live.getHandlerType(input)) {
+                        case 'subscription':
+                            return hasSubscriptionCallback ? subscriptionCallback(input) : input
+                        case 'trigger':
+                            return hasTriggerCallback ? triggerCallback(input) : input
+                        default: 
+                            var message = (window.LiveElement.Scale.Channels[processorName] || []).shift()
+                            return message && typeof message == 'object' && message.payload && typeof message.payload == 'object' ? message.payload : {}
+                    }
+                }
+            } else {
+                return function() {
+                    var message = (window.LiveElement.Scale.Channels[processorName] || []).shift()
+                    return message && typeof message == 'object' && message.payload && typeof message.payload == 'object' ? message.payload : {}
+                }
+            }
+        }
+    }}
 })
 if ('serviceWorker' in window.navigator) {
     window.navigator.serviceWorker.register('worker.js')
     window.navigator.serviceWorker.addEventListener('message', event => {
         if (window.LiveElement.Scale.Options.Listen) {
-            window.LiveElement.Scale.Queue.push(event.data)
+            if (window.LiveElement.Scale.isValidMessage(event.data)) {
+                if (window.LiveElement.Scale.OpenChannels[event.data.channel]) {
+                    window.LiveElement.Scale.Channels[event.data.channel] = window.LiveElement.Scale.Channels[event.data.channel] || []
+                    window.LiveElement.Scale.Channels[event.data.channel].push(event.data)
+                }
+            }
         }
     })
 }
