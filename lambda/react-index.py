03f478bc6b60edc4cea304aab33cbb73ca304756
David Pitman <david.pitman@cloudouble.com>
2021-02-09 07:15:35 +0000

    in progress

--- lambda/react-index.py
+++ lambda/react-index.py
@@ -1,33 +1,50 @@
 import json, boto3, os, time
 
 def main(event, context):
-    #reacts to updated record query indexes _/query/{class_name}/{query_id}/{index}.json
-    # ** regenerate dependent views in all connections
+    '''
+    - triggered by new/updated objects at /query/{class_name}/{query_id}/{record_initial}.json
+    - uses /feed/{class_name}/{query_id}/{connection_id}.json to find affected connections
+    - triggers mask.py for each record_id in the index that doesn't already exist in the /connection/{connection_id}/record/{class_name}/{record_id}.json
+    '''
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(os.environ['bucket'])
     s3_client = boto3.client('s3')
     lambda_client = boto3.client('lambda')
     counter = 0
     now = time.time()
+    affected_connections = set()
     for record in event['Records']:
         path = event['s3']['object']['key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')
+        index_record_ids = set(json.loads(bucket.get_object(Key=event['s3']['object']['key'])['Body'].read().decode('utf-8')))
         if len(path) == 4:
             class_name, query_id, index = path[1:]
             # '/_/feed/{class_name}/{query_id}/{connection_id}.json'
             feed_base_key = '_/feed/{class_name}/{query_id}/'.format(class_name=class_name, query_id=query_id)
             feed_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=feed_base_key)
             for feed_entry in feed_list_response['Contents']:
-                feed_connection_id = feed_entry['Key'].removesuffix('.json').split('/')[-1]
-                feed_data = json.loads(bucket.get_object(Key=feed_entry['Key'])['Body'].read().decode('utf-8'))
-                # {expires, max, next, last, count, views=[{id, sort_field, sort_direction, min_index, max_index}]}
-                active = True
-                ready = True
-                if (feed_data.get('expires') and feed_data['expires'] > now) or (feed_data.get('max') and feed_data.get('count') and feed_data['max'] > feed_data['count']):
-                    active = False
-                if feed_data.get('next') and feed_data.get('next') > now:
-                    ready = False
-                if active and ready and feed_data.get('views'):
-                    lambda_client.invoke(FunctionName='view', InvocationType='Event', Payload=bytes(json.dumps({
-                        'connection_id': feed_connection_id, 'class_name': class_name, 'query_id': query_id, 'views': feed_data['views']}), 'utf-8'))
-                    counter = counter + 1
+                affected_connections.add(feed_entry['Key'].removesuffix('.json').split('/')[-1])
+            c = 1000000000
+            while c and feed_list_response.get('IsTruncated') and feed_list_response.get('NextContinuationToken'):
+                feed_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=feed_base_key, ContinuationToken=feed_list_response.get('NextContinuationToken'))
+                for key_obj in feed_list_response['Contents']:
+                    affected_connections.add(feed_entry['Key'].removesuffix('.json').split('/')[-1])
+                    c = c - 1
+            for affected_connection in affected_connections:
+                connection_records_base_key = '_/connection/{connection_id}/record/{class_name}/'.format(connection_id=affected_connection, class_name=class_name)
+                class_name_record_ids = set()
+                connection_records_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=connection_records_base_key)
+                for record_entry in connection_records_list_response['Contents']:
+                    class_name_record_ids.add(record_entry['Key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')[4])
+                c = 1000000000
+                while c and connection_records_list_response.get('IsTruncated') and connection_records_list_response.get('NextContinuationToken'):
+                    connection_records_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=connection_records_base_key, ContinuationToken=feed_list_response.get('NextContinuationToken'))
+                    for record_entry in connection_records_list_response['Contents']:
+                        class_name_record_ids.add(record_entry['Key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')[4])
+                for connection_record_id in index_record_ids.difference(class_name_record_ids):
+                    lambda_client.invoke(FunctionName='mask', InvocationType='Event', Payload=bytes(json.dumps({
+                        'connection_id': affected_connection,
+                        'class_name': class_name, 
+                        'record_id': connection_record_id, 
+                    }), 'utf-8'))
+            counter = counter + 1
     return counter
--- lambda/react-version.py
+++ lambda/react-version.py
@@ -1,25 +1,51 @@
 import json, boto3, os
 
+def process_entity(key_obj, bucket, lambda_client, entity_type):
+    entity_path = key_obj['Key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')
+    class_name, entity_id, connection_id = entity_path[1:]
+    lambda_client.invoke(FunctionName='mask', InvocationType='Event', Payload=bytes(json.dumps({
+        'connection_id': connection_id,
+        'class_name': class_name, 
+        '{}_id'.format(entity_type): entity_id, 
+    }), 'utf-8'))
+
 def main(event, context):
-    #reacts to updated record version objects _/version/{class_name}/{record_id}/{version_id}.json
-    # ** build related index of queries that include this record
+    '''
+    - triggered by new objects at /version/{class_name}/{record_id}/{version_id}.json
+    - uses /vector/{class_name}/{field_name}.json to find affected queries 
+    - triggers query-record.py with the full record for each affected query
+    - uses /subscription/{class_name}/{record_id}/{connection_id}.json to find affected connections
+    - triggers mask.py for each affected connection
+    '''
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(os.environ['bucket'])
+    s3_client = boto3.client('s3')
     lambda_client = boto3.client('lambda')
     counter = 0
     for record in event['Records']:
         path = event['s3']['object']['key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')
         if len(path) == 4:
-            record_type, record_id, record_version = path[1:]
-            record_data = json.loads(bucket.get_object(Key='_/record/{record_type}/{record_id}.json'.format(record_type=record_type, record_id=record_id))['Body'].read().decode('utf-8'))
+            class_name, record_id, record_version = path[1:]
+            record_data = json.loads(bucket.get_object(Key='_/record/{class_name}/{record_id}.json'.format(class_name=class_name, record_id=record_id))['Body'].read().decode('utf-8'))
             updated_fields = sorted(json.loads(bucket.get_object(Key=record['s3']['object']['key'])['Body'].read().decode('utf-8')))
             query_list = []
             for field_name in updated_fields:
                 try:
-                    query_list.entend(json.loads(bucket.get_object(Key='_/vector/{record_type}/{field_name}.json'.format(record_type=record_type, field_name=field_name))['Body'].read().decode('utf-8')))
+                    query_list.entend(json.loads(bucket.get_object(Key='_/vector/{class_name}/{field_name}.json'.format(class_name=class_name, field_name=field_name))['Body'].read().decode('utf-8')))
                 except:
                     pass
             query_list = sorted(list(set(query_list)))
             for query_id in query_list:
                 lambda_client.invoke(FunctionName='query-record', InvocationType='Event', Payload=bytes(json.dumps({'query': query_id, 'record': record_data}), 'utf-8'))
+            subscription_list_key = '_/subscription/{class_name}/{record_id}/'.format(class_name=class_name, record_id=record_id)
+            subscription_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=subscription_list_key)
+            for key_obj in subscription_list_response['Contents']:
+                process_entity(key_obj, bucket, lambda_client, 'record')
+            c = 1000000000
+            while c and subscription_list_response.get('IsTruncated') and subscription_list_response.get('NextContinuationToken'):
+                subscription_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=subscription_list_key, ContinuationToken=subscription_list_response.get('NextContinuationToken'))
+                for key_obj in subscription_list_response['Contents']:
+                    process_entity(key_obj, bucket, lambda_client, 'record')
+                    c = c - 1
+            counter = counter + 1
     return counter
