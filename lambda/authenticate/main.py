c46a5f6d335a3c49a4277775c74a8a3907e479a2
David Pitman <david.pitman@cloudouble.com>
2021-02-12 07:20:43 +0000

    write passed connection test

--- lambda/authenticate/main.py
+++ lambda/authenticate/main.py
@@ -1,12 +1,12 @@
-import json, boto3, os
+env = {"bucket": "scale.live-element.net", "lambda_namespace": "liveelement-scale", "system_root": "_"}
 
+import json, boto3
 
-def uuid_valid(s):
-    try:
-        uuid.UUID(s).version == 4
-    except:
-        return False
-    return True
+def getpath(p):
+    p = p.strip('/?')
+    p = p[len(env['system_root']):] if p.startswith(env['system_root']) else p
+    p = p[:len('.json')] if p.endswith('.json') else p
+    return p.strip('/').split('/')
 
  
 def main(event, context):
@@ -17,16 +17,16 @@ def main(event, context):
     event => {authentication_channel_name: {credentials}}
     '''
     s3 = boto3.resource('s3')
-    bucket = s3.Bucket(os.environ['bucket'])
+    bucket = s3.Bucket(env['bucket'])
     s3_client = boto3.client('s3')
     lambda_client = boto3.client('lambda')
     connection_record = {'mask': {}}
     
     system_configuration = {'authentication': []}
-    system_authentication_base_key = '_/system/authentication/'
-    system_authentication_list_response = s3_client.list_objects_v2(Bucket=os.environ['bucket'], Prefix=system_authentication_base_key)
+    system_authentication_base_key = '{system_root}/system/authentication/'.format(system_root=env['system_root'])
+    system_authentication_list_response = s3_client.list_objects_v2(Bucket=env['bucket'], Prefix=system_authentication_base_key)
     for authentication_module_entry in system_authentication_list_response['Contents']:
-        module_name = authentication_module_entry['Key'].strip('/?').removeprefix('_').removesuffix('.json').strip('/').split('/')[-1]
+        module_name =  getpath(authentication_module_entry['Key'])[-1]
         system_configuration['authentication'].append({module_name: json.loads(bucket.Object(authentication_module_entry['Key']).get()['Body'].read().decode('utf-8'))})
     system_configuration['authentication'].sort(key=lambda a: a.get('priority', 1000))
     for authentication_channel in system_configuration['authentication']:
