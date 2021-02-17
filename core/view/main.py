aa2ed13163f67771d8b5521eeb305f96af7a110d
David Pitman <david.pitman@cloudouble.com>
2021-02-17 11:16:26 +0000

    view tested

--- core/view/main.py
+++ core/view/main.py
@@ -1,6 +1,5 @@
 env = {"bucket": "scale.live-element.net", "lambda_namespace": "liveelement-scale", "system_root": "_"}
 
-
 import json, boto3, time
 
 def chunks(lst, n):
@@ -19,7 +18,7 @@ def main(event, context):
     s3_client = boto3.client('s3')
     lambda_client = boto3.client('lambda')
     counter = 0
-    if all([event.get(k for k in ['connection_id', 'class_name', 'entity_type', 'entity_id']]) and type(event.get('view')) is dict:
+    if all([event.get(k) for k in ['connection_id', 'class_name', 'entity_type', 'entity_id']]) and type(event.get('view')) is dict:
         connection_id, class_name, entity_type, entity_id, view_local = [event[k] for k in ['connection_id', 'class_name', 'entity_type', 'entity_id', 'view']]
         if view_local.get('view'):
             view_system = json.loads(bucket.Object('{system_root}/view/{view_id}.json'.format(system_root=env['system_root'], view_id=view_local['view'])).get()['Body'].read().decode('utf-8'))
@@ -67,17 +66,19 @@ def main(event, context):
                 for record_id in query_record_ids:
                     query_record_data.append(json.loads(bucket.Object('{system_root}/connection/{connection_id}/record/{class_name}/{record_id}.json'.format(system_root=env['system_root'], connection_id=connection_id, class_name=class_name, record_id=record_id)).get()['Body'].read().decode('utf-8')))
                 sort_field = view.get('sort_field', '@id')
-                query_record_data.sort(key=sort_field, reverse=True if view.get('sort_direction', 'ascending') == 'descending' else False)
+                query_record_data.sort(key=lambda r: r[sort_field], reverse=(True if view.get('sort_direction', 'ascending') == 'descending' else False))
                 total_result_count = len(query_record_data) 
                 query_record_data = query_record_data[view.get('min_index'):view.get('max_index')]
                 view_result_count = len(query_record_data) 
-                for page_name, page_results in {'{}-{}'.format(i*1000, i*1000+999): p for i, p in enumerate(list(chunks(query_record_data, 1000)))}:
-                    switches = {'connection_id': connection_id, 'class_name': class_name, 'entity_id': entity_id, 'field_name': field_name, 'sort_field': sort_field, 'page_name': page_name, 'suffix': suffix}
+                for page_name, page_results in {'{}-{}'.format(i*1000, i*1000+999): p for i, p in enumerate(list(chunks(query_record_data, 1000)))}.items():
+                    switches = {'connection_id': connection_id, 'class_name': class_name, 'entity_id': entity_id, 'field_name': field_name, 
+                        'sort_field': sort_field, 'sort_direction': event.get('sort_direction', 'ascending'), 'page_name': page_name, 'suffix': suffix}
                     if field_name:
                         page_object_key = '{system_root}/connection/{connection_id}/feed/{class_name}/{entity_id}/{field_name}/{sort_field}/{sort_direction}/{page_name}.{suffix}'.format(system_root=env['system_root'], **switches)
                     else:
                         page_object_key = '{system_root}/connection/{connection_id}/feed/{class_name}/{entity_id}/-/{sort_field}/{sort_direction}/{page_name}.{suffix}'.format(system_root=env['system_root'], **switches)
-                    bucket.put_object(Body=lambda_client.invoke(FunctionName='{lambda_namespace}-extension-view-{processor}'.format(lambda_namespace=env['lambda_namespace'], processor=view['processor']), Payload=bytes(json.dumps({
+                    
+                    view_return = lambda_client.invoke(FunctionName='{lambda_namespace}-extension-view-{processor}'.format(lambda_namespace=env['lambda_namespace'], processor=view['processor']), Payload=bytes(json.dumps({
                         'options': view['options'], 
                         'assets': view['assets'], 
                         'entity_type': 'query',
@@ -85,6 +86,7 @@ def main(event, context):
                         'page': page_results, 
                         'total_result_count': total_result_count, 
                         'view_result_count': view_result_count
-                    }), 'utf-8'))['Payload'].read(), Key=page_object_key, ContentType=view.get('content_type', 'application/json'))
+                    }), 'utf-8'))['Payload'].read().decode('utf-8')
+                    bucket.put_object(Body=bytes(json.loads(view_return), 'utf-8'), Key=page_object_key, ContentType=view.get('content_type', 'application/json'))
                 counter = counter + 1
     return counter
