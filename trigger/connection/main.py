4a77ecf57870dce9cd07ec84339dee633e2686d2
David Pitman <david.pitman@cloudouble.com>
2021-03-01 07:57:19 +0000

    trigger connection completed

--- trigger/connection/main.py
+++ trigger/connection/main.py
@@ -7,44 +7,30 @@ def getpath(p, env=None):
     p = p[:-len('.json')] if p.endswith('.json') else p
     return p.strip('/').split('/')
 
-def build_env(entry, context):
-    # host/_/connection/{connection_id}.json
-    temp_path = getpath(entry['s3']['object']['key'])
-    if len(temp_path) in [3, 4]:
-        shared = len(temp_path) == 4
-        return {
-            'bucket': entry['s3']['bucket']['name'], 
-            'lambda_namespace': context.function_name.replace('-core-react-connection-record', ''), 
-            'system_root': temp_path[-3],
-            'data_root': '{}/{}'.format(temp_path[-4], temp_path[-3]) if shared else temp_path[-3], 
-            'shared': 1 if shared else 0
-        }
-    else:
-        return {}
+def get_env_context(event, context):
+    env = context.client_context.env if context.client_context and context.client_context.env else event.get('_env', {})
+    env['path'] = getpath(event['key'], env)
+    client_context = base64.b64encode(bytes(json.dumps({'env': env}), 'utf-8')).decode('utf-8')    
+    return env, client_context
+    
 
-
-def main(event_data, context):
+def main(event, context):
     '''
-    - triggered by writes at _/connection/{connection_id}.json
+    - triggered by writes at _/connection/{connection_id}/connect.json
     - deletes all objects under _/connection/{connection_id}/*
     '''
-    s3 = boto3.resource('s3')
     counter = 0
-    for entry in event_data['Records']:
-        env = build_env(entry, context)
-        if not env:
-            continue
-        env['path'] = getpath(entry['s3']['object']['key'], env)
-        client_context = base64.b64encode(bytes(json.dumps({'env': env}), 'utf-8')).decode('utf-8')
-        if env['path'] and len(env['path']) == 2 and env['path'][0] == 'connection':
+    if event.get('key'):
+        env, client_context = get_env_context(event, context)    
+        s3_client = boto3.client('s3')
+        if env['path'] and len(env['path']) == 3 and env['path'][0] == 'connection':
             connection_id = env['path'][1]
-            s3_client = boto3.client('s3')
-            list_response = s3_client.list_objects_v2(Bucket=env['bucket'], Prefix='{system_root}/connection/{connection_id}/'.format(system_root=env['system_root'], connection_id=connection_id))
-            delete_response = s3_client.delete_objects(Bucket=env['bucket'], Delete={'Objects': [{'Key': c['Key']} for c in list_response['Contents']], 'Quiet': True})
+            list_response = s3_client.list_objects_v2(Bucket=env['bucket'], Prefix='{data_root}/connection/{connection_id}/'.format(data_root=env['data_root'], connection_id=connection_id))
+            #delete_response = s3_client.delete_objects(Bucket=env['bucket'], Delete={'Objects': [{'Key': c['Key']} for c in list_response['Contents'] if c['Key'] != event['key']], 'Quiet': True})
             c = 1000
             while c and list_response.get('IsTruncated') and list_response.get('NextContinuationToken'):
-                list_response = s3_client.list_objects_v2(Bucket=env['bucket'], Prefix='{system_root}/connection/{connection_id}/'.format(system_root=env['system_root'], connection_id=connection_id), ContinuationToken=list_response.get('NextContinuationToken'))
-                delete_response = s3_client.delete_objects(Bucket=env['bucket'], Delete={'Objects': [{'Key': c['Key']} for c in list_response['Contents']], 'Quiet': True})
+                list_response = s3_client.list_objects_v2(Bucket=env['bucket'], Prefix='{data_root}/connection/{connection_id}/'.format(data_root=env['data_root'], connection_id=connection_id), ContinuationToken=list_response.get('NextContinuationToken'))
+                #delete_response = s3_client.delete_objects(Bucket=env['bucket'], Delete={'Objects': [{'Key': c['Key']} for c in list_response['Contents'] if c['Key'] != event['key']], 'Quiet': True})
                 c = c - 1
             counter = counter + 1
     return counter
--- trigger/proxy/main.py
+++ trigger/proxy/main.py
@@ -3,7 +3,7 @@ import json, boto3, base64, re
 env = {"bucket": "scale.live-element.net", "lambda_namespace": "liveelement-scale", "system_root": "_", "shared": 0, "authentication_namespace": "liveelementscale"}
 
 triggers = {
-    
+    'connection/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/connect.json': ['connection']
 }
 
 def main(event_data, context):
@@ -17,7 +17,6 @@ def main(event_data, context):
         if entry.get('s3', {}).get('object', {}).get('key'):
             keys.append(entry['s3']['object']['key'].strip('/?'))
     if keys:
-        trigger_re_compiled = {k: re.compile(k) for k in triggers}
         lambda_client = boto3.client('lambda')
     for key in keys:
         if env['shared']:
@@ -28,6 +27,7 @@ def main(event_data, context):
                continue
         else:
            env['data_root'] = env['system_root']
+        trigger_re_compiled = {k: re.compile('{data_root}/{path}'.format(data_root=env['data_root'], path=k.strip('/'))) for k in triggers}
         for key_re, trigger_processors in triggers.items():
             if trigger_re_compiled[key_re].fullmatch(key):
                 for trigger_processor in trigger_processors:
