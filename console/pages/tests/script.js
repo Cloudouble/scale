67f94d05ce07fa789fd5c61659c533be09491be6
David Pitman <david.pitman@cloudouble.com>
2021-04-25 22:17:31 +0000

    progressing on console

--- console/pages/tests/script.js
+++ console/pages/tests/script.js
@@ -1,6 +1,7 @@
 window.LiveElement.Scale.Console.Tests = window.LiveElement.Scale.Console.Tests || {}
 
 window.LiveElement.Scale.Console.Tests.runTest = function(tr, test) {
+    window.localStorage.removeItem(`tests:${tr.getAttribute('name')}:result`)
     var resultLabel = tr.querySelector(':scope > td > label')
     resultLabel.setAttribute('status', 'pending')
     tr.querySelector('code').innerHTML = '...'
@@ -8,11 +9,12 @@ window.LiveElement.Scale.Console.Tests.runTest = function(tr, test) {
     tr.querySelector('time').innerHTML = '...'
     var system_access_url = window.localStorage.getItem('system:system_access_url')
     var system_root = window.localStorage.getItem('system:system_root')
-    var connection_id = installation.getAttribute('connection-id')
+    var connection_id = window.localStorage.getItem(`tests:create-sudo-connection:result`)
     var connection_url = `${system_access_url}${system_root}/connection/${connection_id}`
     return Promise.resolve(test(connection_url, system_access_url, system_root, connection_id)).then(result => {
         tr.querySelector('time').innerHTML = `${Math.round(window.performance.now() - start)}ms`
         resultLabel.setAttribute('status', 'success')
+        window.localStorage.setItem(`tests:${tr.getAttribute('name')}:result`, result)
         return result
     }).catch(e => {
         tr.querySelector('time').innerHTML = `${Math.round(window.performance.now() - start)}ms`
@@ -21,9 +23,16 @@ window.LiveElement.Scale.Console.Tests.runTest = function(tr, test) {
 }
 
 window.setTimeout(() => {
+    var installation = document.getElementById('tests').querySelector('table[name="installation"]')
     Object.entries(testMap).forEach(entry => {
-        installation.querySelector(`[name="${entry[0]}"] button`).addEventListener('click', event => {
-            var tr = event.target.closest('[name]')
+        var resultStorageKey = `test:${entry[0]}:result`
+        var tr = installation.querySelector(`tr[name="${entry[0]}"]`)
+        tr.querySelector('i').innerHTML = entry[0]
+        if (window.localStorage.getItem(resultStorageKey)) {
+            tr.querySelector('label').setAttribute('status', 'success')
+            tr.querySelector('code').innerHTML = window.localStorage.getItem(resultStorageKey)
+        }
+        tr.querySelector('button').addEventListener('click', event => {
             window.LiveElement.Scale.Console.Tests.runTest(tr, entry[1]).then(result => {
                 tr.querySelector('code').innerHTML = result
             }).catch(e => {
@@ -33,28 +42,16 @@ window.setTimeout(() => {
     })
 }, 1)
 
-var tests = document.getElementById('tests')
-var installation = tests.querySelector('table[name="installation"]')
-
-if (window.localStorage.getItem('system:connection_id')) {
-    installation.setAttribute('connection-id', window.localStorage.getItem('system:connection_id'))
-    installation.querySelector('tr[name="create-sudo-connection"] label').setAttribute('status', 'success')
-    installation.querySelector('tr[name="create-sudo-connection"] code').innerHTML = window.localStorage.getItem('system:connection_id')
-}
-
 var testMap = {
     'create-sudo-connection': function(connection_url, system_access_url, system_root, connection_id) {
-        var connection_id = window.LiveElement.Scale.Core.generateUUID4()
+        connection_id = window.LiveElement.Scale.Core.generateUUID4()
         var sudo_key = window.localStorage.getItem('system:sudo_key')
         return window.fetch(`${system_access_url}${system_root}/connection/${connection_id}/connect.json`, 
             {method: 'PUT', headers: {"Content-Type": "application/json"}, body: JSON.stringify(
                 {sudo: {key: sudo_key}}
             ) }
         ).then(r => {
-            installation.setAttribute('connection-id', connection_id)
             return connection_id
-        }).catch(e => {
-            installation.removeAttribute('connection-id')
         })
     }, 
     'create-websocket': function(connection_url, system_access_url, system_root, connection_id) {
@@ -92,13 +89,13 @@ var testMap = {
         })
     }, 
     'delete-record': function(connection_url, system_access_url, system_root, connection_id) {
-        var record_id = installation.querySelector('[name="create-record-put"] code').innerHTML
+        var record_id = window.localStorage.getItem('tests:create-record-put:result')
         if (record_id) {
             return window.fetch(`${connection_url}/record/Book/${record_id}.json`, {method: 'DELETE', headers: {"Content-Type": "application/json"}}).then(r => {
                 return record_id
             })
         } else {
-            return `Error: please first run "${installation.querySelector('[name="create-record-put"] th[scope="row"]').innerText.split('\n').shift()}"`
+            return `Error: please first run "create-record-put"`
         }
     }, 
     'create-record-post': function(connection_url, system_access_url, system_root, connection_id) {
@@ -112,10 +109,18 @@ var testMap = {
     }, 
     'create-subscription': function(connection_url, system_access_url, system_root, connection_id) {
         var subscription_id = window.LiveElement.Scale.Core.generateUUID4()
-        var record_id = installation.querySelector('[name="create-record-post"] code').innerHTML
-        var subscription = {view: '7b35c2bf-34fa-4a41-9fae-fb8465a55520'}
+        var record_id = window.localStorage.getItem('tests:create-record-post:result')
+        var subscription = {view: window.localStorage.getItem('tests:create-view:result')}
         return window.fetch(`${connection_url}/subscription/Book/${record_id}/${subscription_id}.json`, {method: 'PUT', headers: {"Content-Type": "application/json"}, body: JSON.stringify(subscription)}).then(r => {
             return subscription_id
+        })
+    }, 
+    'create-feed': function(connection_url, system_access_url, system_root, connection_id) {
+        var feed_id = window.LiveElement.Scale.Core.generateUUID4()
+        var query_id = window.localStorage.getItem('tests:create-query:result')
+        var feed = {view: window.localStorage.getItem('tests:create-view:result')}
+        return window.fetch(`${connection_url}/feed/Book/${query_id}/${feed_id}.json`, {method: 'PUT', headers: {"Content-Type": "application/json"}, body: JSON.stringify(feed)}).then(r => {
+            return feed_id
         })
     }
 }
