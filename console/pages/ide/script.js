0e2290710d70a8b436b91190e2aae8dbe905b759
David Pitman <david.pitman@cloudouble.com>
2021-05-05 12:40:31 +0000

    working on IDE

--- console/pages/ide/script.js
+++ console/pages/ide/script.js
@@ -15,36 +15,35 @@ window.LiveElement.Live.processors.IdeChannelConfigure = function(input) {
                     elements[n].value = window.LiveElement.Scale.Core.generateUUID4()
                 }
             })
-            var channelId = elements['id'].value
-            var channelObject = {receiveKey: elements.receiveKey.value, sendKey: elements.sendKey.value, adminKey: elements.adminKey.value}
-            window.LiveElement.Scale.Console.IDE.Channel.Configure = {...{name: elements.name.value, id: elements.id.value}, ...channelObject}
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = elements['id'].value
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = {
+                ...{name: elements.name.value}, 
+                ...{receiveKey: elements.receiveKey.value, sendKey: elements.sendKey.value, adminKey: elements.adminKey.value}
+            }
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = elements.id.value
             var nameInput = channel_configure.querySelector('input[name="name"]')
             var buildSnippet = function() {
                 var codeSnippet = channel_configure.querySelector('code')
-                codeSnippet.innerHTML = `
-        window.fetch(
-            '${window.localStorage.getItem('system:system_access_url')}${window.localStorage.getItem('system:system_root')}/channel/${channelId}/connect.json', 
-            {
-                method: 'PUT', 
-                headers: {
-                    "Content-Type": "application/json"
-                }, 
-                body: JSON.stringify({
-                    ${JSON.stringify(channelObject, null, 14).slice(1, -1)}
-                }) 
+                codeSnippet.innerHTML = codeSnippet.innerHTML.replace()
             }
-        )
-                `
-            }
-            nameInput.focus()
+            elements.name.focus()
             buildSnippet()
         } else if ((['name', 'id', 'receiveKey', 'sendKey', 'adminKey']).includes(input.attributes.name)) {
             window.LiveElement.Scale.Console.IDE.Channel.Configure = window.LiveElement.Scale.Console.IDE.Channel.Configure || {}
-            window.LiveElement.Scale.Console.IDE.Channel.Configure[input.attributes.name] = input.properties.value
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = window.LiveElement.Scale.Console.IDE.Channel.Configure.channel || {}
+            if (input.attributes.name == 'id') {
+                window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = input.properties.value
+            } else {
+                window.LiveElement.Scale.Console.IDE.Channel.Configure.channel[input.attributes.name] = input.properties.value
+            }
         }
     }
 
 }
+
+
+window.LiveElement.Live.listeners.IdeChannelConfigure = {processor: 'IdeChannelConfigure'}
+
 
 ide.querySelectorAll('input[type="uuid"]').forEach(uuidInput => {
     uuidInput.setAttribute('pattern', '^[0-9a-z]{8}-[0-9a-z]{4}-4[0-9a-z]{3}-[89ab][0-9a-z]{3}-[0-9a-z]{12}$')
