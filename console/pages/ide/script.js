cd03307389677949695956772b45232d6e975f81
David Pitman <david.pitman@cloudouble.com>
2021-05-06 04:40:44 +0000

    changes for 1.0.4

--- console/pages/ide/script.js
+++ console/pages/ide/script.js
@@ -10,35 +10,79 @@ ide.querySelectorAll('div[name="sidebar"] button').forEach(button => {
     }
 })
 
-window.LiveElement.Live.processors.IdeChannelConfigure = function(input) {
-    if (window.LiveElement.Live.getHandlerType(input) == 'trigger') {
-        var channel_configure = ide.querySelector('fieldset[name="configure"]')
-        if (input.attributes.name == 'new') {
-            var elements = {}
-            ;(['@name', 'id', 'receiveKey', 'sendKey', 'adminKey']).forEach(n => {
-                elements[n] = channel_configure.querySelector(`input[name="${n}"]`)
-                if (n != '@name') {
-                    elements[n].value = window.LiveElement.Scale.Core.generateUUID4()
+window.LiveElement.Live.processors.IdeChannelSearch = function(input) {
+    if (input.attributes.name == 'search') {
+        var event = input.vector.split(':').shift()
+        if (event == 'keyup') {
+            window.LiveElement.Scale.Console.System.invokeLambda({
+                page: 'ide', 
+                entity_type: 'channel', 
+                heading: 'search',
+                search: input.properties.value
+            }).then(searchResult => {
+                if (searchResult && typeof searchResult == 'object' && searchResult.result && typeof searchResult.result == 'object') {
+                    window.LiveElement.Scale.Console.IDE.Channel.Search = searchResult.result
+                    var datalist = document.getElementById('ide-channel-search-list')
+                    datalist.innerHTML = ''
+                    Object.keys(window.LiveElement.Scale.Console.IDE.Channel.Search).sort().forEach(channel_id => {
+                        var optionElement = document.createElement('option')
+                        optionElement.setAttribute('value', channel_id)
+                        if (window.LiveElement.Scale.Console.IDE.Channel.Search[channel_id]['@name']) {
+                            optionElement.innerHTML = `${channel_id} (@${window.LiveElement.Scale.Console.IDE.Channel.Search[channel_id]['@name']})`
+                        } else {
+                            optionElement.innerHTML = `${channel_id}`
+                        }
+                        datalist.appendChild(optionElement)
+                    })
                 }
             })
-            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = elements['id'].value
-            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = {
-                ...{'@name': elements['@name'].value}, 
-                ...{receiveKey: elements.receiveKey.value, sendKey: elements.sendKey.value, adminKey: elements.adminKey.value}
+        }
+    }
+}
+
+window.LiveElement.Live.processors.IdeChannelConfigure = function(input) {
+    var setupConfigure = function(configureElement) {
+        var elements = {}
+        ;(['@name', 'id', 'receiveKey', 'sendKey', 'adminKey']).forEach(n => {
+            elements[n] = configureElement.querySelector(`input[name="${n}"]`)
+            if (n == 'id') {
+                elements[n].value = window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id || ''
+            } else {
+                elements[n].value = window.LiveElement.Scale.Console.IDE.Channel.Configure.channel[n] || ''
             }
-            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = elements.id.value
-            var nameInput = channel_configure.querySelector('input[name="@name"]')
-            elements['@name'].focus()
-            window.LiveElement.Scale.Core.buildSnippet(channel_configure.querySelector('code'))
+        })
+        window.LiveElement.Scale.Core.buildSnippet(configureElement.querySelector('code'))
+    }
+    if (window.LiveElement.Live.getHandlerType(input) == 'trigger') {
+        var configureElement = ide.querySelector('fieldset[name="configure"]')
+        if (input.attributes.name == 'new') {
+            configureElement.setAttribute('mode', 'new')
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = window.LiveElement.Scale.Core.generateUUID4()
+            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = {
+                receiveKey: window.LiveElement.Scale.Core.generateUUID4(), 
+                sendKey: window.LiveElement.Scale.Core.generateUUID4(), 
+                adminKey: window.LiveElement.Scale.Core.generateUUID4()
+            }
+            setupConfigure(configureElement)
+            configureElement.querySelector('input[name="@name"]').focus()
         } else if ((['@name', 'id', 'receiveKey', 'sendKey', 'adminKey']).includes(input.attributes.name)) {
-            window.LiveElement.Scale.Console.IDE.Channel.Configure = window.LiveElement.Scale.Console.IDE.Channel.Configure || {}
             window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = window.LiveElement.Scale.Console.IDE.Channel.Configure.channel || {}
             if (input.attributes.name == 'id') {
                 window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = input.properties.value
             } else {
                 window.LiveElement.Scale.Console.IDE.Channel.Configure.channel[input.attributes.name] = input.properties.value
             }
-            window.LiveElement.Scale.Core.buildSnippet(channel_configure.querySelector('code'))
+            setupConfigure(configureElement)
+        } else if (input.attributes.name == 'load') {
+            configureElement.setAttribute('mode', 'load')
+            var searchElement = ide.querySelector('fieldset[name="search"] input[name="search"]')
+            if (searchElement) {
+                if (window.LiveElement.Scale.Console.IDE.Channel.Search && window.LiveElement.Scale.Console.IDE.Channel.Search[searchElement.value]) {
+                    window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = searchElement.value
+                    window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = window.LiveElement.Scale.Console.IDE.Channel.Search[searchElement.value]
+                    setupConfigure(configureElement)
+                }
+            }
         }
     }
 
