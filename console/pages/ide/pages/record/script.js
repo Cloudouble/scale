2d2b6f07d530cda158f6df6001f467a19c30426c
David Pitman <david.pitman@cloudouble.com>
2021-05-11 15:11:59 +0000

    working on IDE

--- console/pages/ide/pages/record/script.js
+++ console/pages/ide/pages/record/script.js
@@ -64,14 +64,103 @@ window.LiveElement.Live.processors.IdeRecordEdit = function(input) {
     if (handlerType == 'subscription') {
         var editor = input.subscriber.querySelector('div.editor')
         editor.innerHTML = ''
-        Object.keys(input.payload).sort().forEach(field => {
+        Object.keys(input.payload).sort().forEach(property => {
             var labelElement = document.createElement('label')
-            labelElement.innerHTML = field
-            var inputElement = document.createElement('input')
-            inputElement.value = input.payload[field]
-            labelElement.appendChild(inputElement)
+            labelElement.setAttribute('name', property)
+
+            var propertyElement = document.createElement('input')
+            propertyElement.setAttribute('name', 'property')
+            propertyElement.setAttribute('type', 'search')
+            propertyElement.setAttribute('list', 'ide-record-edit-properties-list')
+            propertyElement.value = property
+            labelElement.appendChild(propertyElement)
+            
+            var listElementId = window.LiveElement.Scale.Core.generateUUID4()
+            var listElement = document.createElement('datalist')
+            listElement.setAttribute('name', 'types')
+            listElement.setAttribute('id', listElementId)
+            labelElement.appendChild(listElement)
+
+            var typeElement = document.createElement('input')
+            typeElement.setAttribute('name', 'type')
+            typeElement.setAttribute('list', listElementId)
+            labelElement.appendChild(typeElement)
+
+            var valueElement = document.createElement('input')
+            valueElement.setAttribute('name', 'value')
+            valueElement.value = input.payload[property]
+            labelElement.appendChild(valueElement)
+            
             editor.appendChild(labelElement)
         })
+        var injectDataTypes = function() {
+            var properties = window.LiveElement.Scale.Console.IDE.Record.Edit.class[window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type']].properties
+            editor.querySelectorAll('label').forEach(label => {
+                var property = label.getAttribute('name')
+                var types
+                if (property in properties) {
+                    types = properties[property].types
+                } else {
+                    types = ['Text']
+                }
+                var datalist = label.querySelector('datalist[name="types"]')
+                datalist.innerHTML = ''
+                types.sort().forEach(type => {
+                    var option = document.createElement('option')
+                    option.setAttribute('value', type)
+                    option.innerHTML = window.LiveElement.Scale.Console.IDE.Record.Edit.datatype[type] 
+                        ? window.LiveElement.Scale.Console.IDE.Record.Edit.datatype[type].label 
+                        : type
+                    datalist.appendChild(option)
+                })
+            })
+        }
+        var injectProperties = function() {
+            var listElement = document.getElementById('ide-record-edit-properties-list')
+            if (window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type'] != listElement.getAttribute('class')) {
+                listElement.innerHTML = ''
+                listElement.setAttribute('class', window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type'])
+                var properties = window.LiveElement.Scale.Console.IDE.Record.Edit.class[window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type']].properties
+                Object.keys(properties).sort().forEach(property_name => {
+                    var optionElement = document.createElement('option')
+                    optionElement.setAttribute('value', property_name)
+                    optionElement.innerHTML = properties[property_name].label
+                    listElement.appendChild(optionElement)
+                })
+                if (!window.LiveElement.Scale.Console.IDE.Record.Edit.datatype) {
+                    window.LiveElement.Scale.Console.System.invokeLambda({
+                        page: 'ide', 
+                        entity_type: 'record', 
+                        heading: 'edit',
+                        datatypes: true
+                    }).then(dataTypes => {
+                        if (dataTypes && typeof dataTypes == 'object') {
+                            window.LiveElement.Scale.Console.IDE.Record.Edit.datatype = dataTypes
+                            injectDataTypes()
+                        }
+                    })
+                } else {
+                    injectDataTypes()
+                }
+            }
+        }
+        window.LiveElement.Scale.Console.IDE.Record.Edit.class = window.LiveElement.Scale.Console.IDE.Record.Edit.class || {}
+        if (!window.LiveElement.Scale.Console.IDE.Record.Edit.class[window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type']]) {
+            window.LiveElement.Scale.Console.System.invokeLambda({
+                page: 'ide', 
+                entity_type: 'record', 
+                heading: 'edit',
+                record_type: window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type']
+            }).then(classDefinition => {
+                if (classDefinition && typeof classDefinition == 'object') {
+                    window.LiveElement.Scale.Console.IDE.Record.Edit.class[window.LiveElement.Scale.Console.IDE.Record.Edit.record['@type']] = classDefinition
+                    injectProperties()
+                }
+            })
+        } else {
+            injectProperties()
+        }
+        
     } else if (handlerType == 'trigger') {
         console.log('line 67', input)
     }
--- core/console/main.py
+++ core/console/main.py
@@ -117,10 +117,80 @@ def main(event, context):
                     retval = run_test('system/daemon/{module_id}.json'.format(module_id=context['create-daemon']), 'state', 'removed', start_time, s3_client)
                 elif test == 'create-418-error':
                     retval = run_test('error/418.html', None, result, start_time, s3_client)
+                elif test == 'create-custom-class':
+                    time.sleep(10)
+                    retval = run_test('system/class/{class_name}.json'.format(class_name=result), 'label', result, start_time, s3_client)
+                elif test == 'create-custom-record':
+                    retval = run_test('record/Novel/{record_id}.json'.format(record_id=result), None, result, start_time, s3_client)
+                elif test == 'install-schema':
+                    retval = run_test('system/schema/{schema_id}.json'.format(schema_id=result), '@context', dict, start_time, s3_client)
+                elif test == 'uninstall-schema':
+                    retval = run_test('system/schema/{schema_id}.json'.format(schema_id=result), False, result, start_time, s3_client)
+                elif test == 'install-package':
+                    retval = run_test('system/package/{package_id}.json'.format(package_id=result), None, None, start_time, s3_client)
+                elif test == 'uninstall-package':
+                    retval = run_test('system/package/{package_id}.json'.format(package_id=result), False, result, start_time, s3_client)
+                elif test == 'build-snapshot':
+                    retval = run_test('system/snapshot/{snapshot_id}.json'.format(snapshot_id=result), None, None, start_time, s3_client)
+            elif page == 'ide':
+                entity_type = event.get('entity_type')
+                heading = event.get('heading')
+                if entity_type == 'channel': 
+                    if heading == 'search':
+                        search = event.get('search')
+                        retval = {'search': search, 'result': {}}
+                        if search:
+                            channels = s3_client.list_objects_v2(Bucket=env['bucket'], MaxKeys=1000,  Prefix='{data_root}/channel/{search}'.format(data_root=env['data_root'], search=search))['Contents']
+                            try:
+                                channels = {c['Key'].split('/')[-2]: json.loads(s3_client.get_object(Bucket=env['bucket'], Key=c['Key'])['Body'].read().decode('utf-8')) for c in channels if c['Key'].endswith('/connect.json')}
+                            except:
+                                channels = {}
+                            retval['result'] = channels
+                elif entity_type == 'asset': 
+                    if heading == 'search':
+                        search = event.get('search')
+                        retval = {'search': search, 'result': {}}
+                        if search:
+                            try:
+                                assets = s3_client.list_objects_v2(Bucket=env['bucket'], MaxKeys=100,  Prefix='{data_root}/asset/{search}'.format(data_root=env['data_root'], search=search))['Contents']
+                                try:
+                                    assets = {a['Key'].replace('{}/{}'.format(env['data_root'], 'asset'), '', 1).strip('/'): {k: v.timestamp() if k == 'LastModified' else v for k, v in s3_client.head_object(Bucket=env['bucket'], Key=a['Key']).items() if k in ['ContentType', 'ContentLength', 'LastModified'] } for a in assets}
+                                except:
+                                    assets = {}
+                            except:
+                                assets = {}
+                        retval['result'] = assets
+                elif entity_type == 'record':
+                    record_type = event.get('record_type')
+                    if heading == 'search':
+                        input_name = event.get('input_name')
+                        search = event.get('search')
+                        if input_name and search:
+                            retval = {'search': search, 'result': []} 
+                            prefix = None
+                            if input_name in ['search-type', 'search-uuid']:
+                                if input_name == 'search-type':
+                                    prefix = '{data_root}/system/class/{search}'.format(data_root=env['data_root'], search=search)
+                                elif input_name == 'search-uuid' and record_type:
+                                    prefix = '{data_root}/record/{record_type}/{search}'.format(data_root=env['data_root'], record_type=record_type, search=search)
+                                if prefix:
+                                    retval['result'] = s3_client.list_objects_v2(Bucket=env['bucket'], MaxKeys=1000,  Prefix=prefix)['Contents']
+                                    try:
+                                        retval['result'] = [e['Key'].split('/')[-1].replace('.json', '') for e in retval['result']]
+                                    except:
+                                        retval['result'] = []
+                        elif input_name == 'load' and record_type and event.get('record_uuid'):
+                            record_uuid = event['record_uuid']
+                            record = json.loads(s3_client.get_object(Bucket=env['bucket'], Key='{data_root}/record/{record_type}/{record_uuid}.json'.format(data_root=env['data_root'], record_type=record_type, record_uuid=record_uuid))['Body'].read().decode('utf-8'))
+                            retval = record
+                    elif heading == 'edit':
+                        class_definition = json.loads(s3_client.get_object(Bucket=env['bucket'], Key='{data_root}/system/class/{record_type}.json'.format(data_root=env['data_root'], record_type=record_type))['Body'].read().decode('utf-8'))
+                        properties = class_definition.get('properties', {})
+                        for property_name, property_types in properties.items():
+                            property_definition = json.loads(s3_client.get_object(Bucket=env['bucket'], Key='{data_root}/system/property/{property_name}.json'.format(data_root=env['data_root'], property_name=property_name))['Body'].read().decode('utf-8'))
+                            class_definition['properties'][property_name] = {'label': property_definition.get('comment', ''), 'types': property_types}
+                        retval = class_definition
 
-                    
-                            
-                    
+                        
 
-    
     return retval
