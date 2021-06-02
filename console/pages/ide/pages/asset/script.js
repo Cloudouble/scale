2f5c352be57daae53a2e471a423fda5ce1d4b908
David Pitman <david.pitman@cloudouble.com>
2021-06-02 02:45:51 +0000

    working on IDE

--- console/pages/ide/pages/asset/script.js
+++ console/pages/ide/pages/asset/script.js
@@ -8,12 +8,13 @@ window.LiveElement.Live.processors.IdeAssetEdit = function(input) {
                 window.LiveElement.Scale.Console.IDE.Asset.assetElement.remove()
             }
             window.LiveElement.Scale.Console.IDE.Asset.assetElement = document.createElement('element-asset')
+            editFieldset.querySelector('h3').after(window.LiveElement.Scale.Console.IDE.Asset.assetElement)
             window.LiveElement.Scale.Console.IDE.Asset.assetElement.mode = 'editor'
+            //Object.keys(input.entity).sort().forEach(k => window.LiveElement.Scale.Console.IDE.Asset.assetElement[k] = input.entity[k])
             Object.assign(window.LiveElement.Scale.Console.IDE.Asset.assetElement, input.entity)
             window.LiveElement.Scale.Console.IDE.Asset.assetElement.addEventListener('change', event => {
                 window.LiveElement.Scale.Console.buildSnippets('asset')
             })
-            editFieldset.querySelector('h3').after(window.LiveElement.Scale.Console.IDE.Asset.assetElement)
         }
     }
 }
