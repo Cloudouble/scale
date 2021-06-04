9ef7877a532a9e8869080db71203d51cbfbef7f3
David Pitman <david.pitman@cloudouble.com>
2021-06-04 09:05:31 +0000

    working on IDE

--- console/pages/ide/pages/channel/script.js
+++ console/pages/ide/pages/channel/script.js
@@ -11,10 +11,24 @@ window.LiveElement.Live.processors.IdeChannelEdit = function(input) {
             sendKey: (window.LiveElement.Scale.Console.IDE.Channel.channelElement || {}).sendKey
         }
     } else if (handlerType == 'trigger') {
+        var cleanUp = function() {
+            if (window.LiveElement.Scale.Console.IDE.Channel.channelElement) {
+                window.LiveElement.Scale.Console.IDE.Channel.channelElement.remove()
+                delete window.LiveElement.Scale.Console.IDE.Channel.channelElement
+            }
+            if (window.LiveElement.Scale.Console.IDE.Channel.Test.socket) {
+                if (window.LiveElement.Scale.Console.IDE.Channel.Test.socket.readyState && window.LiveElement.Scale.Console.IDE.Channel.Test.socket.readyState < 2) {
+                    window.LiveElement.Scale.Console.IDE.Channel.Test.socket.close(1001)
+                }
+                delete window.LiveElement.Scale.Console.IDE.Channel.Test.socket
+            }
+            testFieldset.querySelector('textarea').value = ''
+        }
         if (input.entity) {
+            cleanUp()
             editFieldset.setAttribute('active', true)
             codeFieldset.setAttribute('active', true)
-            //testFieldset.setAttribute('active', true)
+            testFieldset.setAttribute('active', true)
             window.LiveElement.Scale.Console.IDE.Channel.channelElement = editFieldset.querySelector('element-channel')
             if (window.LiveElement.Scale.Console.IDE.Channel.channelElement) {
                 window.LiveElement.Scale.Console.IDE.Channel.channelElement.remove()
@@ -28,11 +42,11 @@ window.LiveElement.Live.processors.IdeChannelEdit = function(input) {
             })
             window.LiveElement.Scale.Console.IDE.Channel.channelElement.setAttribute('live-subscription', 'IdeChannelEdit:IdeChannelCode')
             window.LiveElement.Live.listen(window.LiveElement.Scale.Console.IDE.Channel.channelElement, 'IdeChannelEdit', 'change', false, true)
-            window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')            
         } else {
+            cleanUp()
             editFieldset.removeAttribute('active')
             codeFieldset.removeAttribute('active')
-            //testFieldset.removeAttribute('active')
+            testFieldset.removeAttribute('active')
         }
     }
 }
@@ -56,173 +70,40 @@ window.LiveElement.Live.processors.IdeChannelCode = function(input) {
             }
         })
     }
-    
 }
 
-window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')
-
-window.LiveElement.Live.listeners.IdeChannelEdit = {processor: 'IdeChannelEdit', expired: true}
-//window.LiveElement.Live.listeners.IdeChannelTest = {processor: 'IdeChannelTest', expired: true}
-
-
-
-
-
-/*window.LiveElement.Live.processors.IdeChannelSearch = function(input) {
-    var handlerType = window.LiveElement.Live.getHandlerType(input)
-    if (handlerType == 'listener') {
-        return {...window.LiveElement.Scale.Console.IDE.Channel.Configure.channel, ...{id : window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}}
-    } else if (window.LiveElement.Live.getHandlerType(input) == 'trigger') {
-        if (input.attributes.name == 'search') {
-            window.LiveElement.Scale.Console.System.invokeLambda({
-                page: 'ide', 
-                entity_type: 'channel', 
-                heading: 'search',
-                search: input.properties.value
-            }).then(searchResult => {
-                if (searchResult && typeof searchResult == 'object' && searchResult.result && typeof searchResult.result == 'object') {
-                    window.LiveElement.Scale.Console.IDE.Channel.Search = searchResult.result
-                    var datalist = document.getElementById('ide-channel-search-list')
-                    datalist.innerHTML = ''
-                    Object.keys(window.LiveElement.Scale.Console.IDE.Channel.Search).sort().forEach(channel_id => {
-                        var optionElement = document.createElement('option')
-                        optionElement.setAttribute('value', channel_id)
-                        if (window.LiveElement.Scale.Console.IDE.Channel.Search[channel_id]['@name']) {
-                            optionElement.innerHTML = `${channel_id} (@${window.LiveElement.Scale.Console.IDE.Channel.Search[channel_id]['@name']})`
-                        } else {
-                            optionElement.innerHTML = `${channel_id}`
-                        }
-                        datalist.appendChild(optionElement)
-                    })
-                }
-            })
-        } else if (input.attributes.name == 'load' || input.attributes.name == 'new') {
-            var searchFieldset = input.triggersource.closest('fieldset')
-            if (input.attributes.name == 'new') {
-                window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = window.LiveElement.Scale.Core.generateUUID4()
-                window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = {
-                    receiveKey: window.LiveElement.Scale.Core.generateUUID4(), 
-                    sendKey: window.LiveElement.Scale.Core.generateUUID4(), 
-                    adminKey: window.LiveElement.Scale.Core.generateUUID4()
-                }
-            } else {
-                var searchElement = searchFieldset.querySelector('input[name="search"]')
-                if (window.LiveElement.Scale.Console.IDE.Channel.Search && window.LiveElement.Scale.Console.IDE.Channel.Search[searchElement.value]) {
-                    window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id = searchElement.value
-                    window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = window.LiveElement.Scale.Console.IDE.Channel.Search[searchElement.value]
-                }
-            }
-            searchFieldset.dispatchEvent(new window.CustomEvent('loaded'))
-        }
-    }
-}*/
-
-
-/*window.LiveElement.Live.processors.IdeChannelConfigure = function(input) {
-    var handlerType = window.LiveElement.Live.getHandlerType(input)
-    if (handlerType == 'listener') {
-        return {...window.LiveElement.Scale.Console.IDE.Channel.Configure.channel, ...{id : window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}}
-    } else if (handlerType == 'subscription') {
-        var complete = true
-        input.subscriber.querySelectorAll('input[name]').forEach(i => {
-            if (i.name in input.payload) {
-                i.value = input.payload[i.name] || ''
-            } else {
-                i.value = ''
-                if (i.name != '@name') {
-                    complete = false
-                }
-            }
-        })
-        window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')
-        if (complete) {
-            input.subscriber.setAttribute('mode', window.LiveElement.Scale.Console.IDE.Channel.Search && window.LiveElement.Scale.Console.IDE.Channel.Search[input.payload.id] ? 'load' : 'new')
-        } else {
-            input.subscriber.removeAttribute('mode')
-        }
-        if (input.subscriber.getAttribute('mode') == 'new') {
-            input.subscriber.querySelector('input[name="@name"]').value = ''
-        }
-    } else if (window.LiveElement.Live.getHandlerType(input) == 'trigger') {
-        if (input.attributes.name == '@name') {
-            window.LiveElement.Scale.Console.IDE.Channel.Configure.channel['@name'] = input.triggersource.value
-            window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')
-        } else if (input.attributes.name == 'create') {
-            window.fetch(
-                `${window.localStorage.getItem('system:system_access_url')}${window.localStorage.getItem('system:system_root')}/connection/${window.localStorage.getItem('system:connection_id')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}/connect.json`, 
-                {
-                    method: 'PUT', 
-                    headers: {
-                        "Content-Type": "application/json"
-                    }, 
-                    body: JSON.stringify(window.LiveElement.Scale.Console.IDE.Channel.Configure.channel) 
-                }
-            )
-        } else if (input.attributes.name == 'delete') {
-            window.fetch(
-                `${window.localStorage.getItem('system:system_access_url')}${window.localStorage.getItem('system:system_root')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.adminKey}`, 
-                {
-                    method: 'DELETE', 
-                }
-            ).then(() => {
-                delete window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id
-                window.LiveElement.Scale.Console.IDE.Channel.Configure.channel = {}
-                var searchFieldset = window.LiveElement.Scale.Console.IDE.pageElement.querySelector('section[name="channel"] fieldset[name="search"]')
-                input.triggersource.closest('fieldset').removeAttribute('mode')                
-                var searchInput = searchFieldset.querySelector('input')
-                searchInput.value = ''
-                searchInput.focus()
-                searchFieldset.dispatchEvent(new window.CustomEvent('loaded'))
-            })
-        }
-    }
-}
-window.LiveElement.Live.processors.IdeChannelCode = function(input) {
-    var handlerType = window.LiveElement.Live.getHandlerType(input)
-    if (handlerType == 'listener') {
-        return window.LiveElement.Scale.Console.IDE.Channel.Configure
-    } else if (handlerType == 'subscription') {
-        window.LiveElement.Scale.Console.IDE.Channel.Code = {
-            receive_url: window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.receiveKey ? `${window.localStorage.getItem('system:system_access_url').replace('https:', 'wss:')}${window.localStorage.getItem('system:system_root')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.receiveKey}` : undefined, 
-            send_url: window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.sendKey ? `${window.localStorage.getItem('system:system_access_url')}${window.localStorage.getItem('system:system_root')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.sendKey}` : undefined, 
-            admin_url: window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.adminKey ? `${window.localStorage.getItem('system:system_access_url')}${window.localStorage.getItem('system:system_root')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel_id}/${window.LiveElement.Scale.Console.IDE.Channel.Configure.channel.adminKey}` : undefined
-        }
-        input.subscriber.querySelectorAll('input[name]').forEach(i => {
-            if (window.LiveElement.Scale.Console.IDE.Channel.Code[i.name]) {
-                i.value = window.LiveElement.Scale.Console.IDE.Channel.Code[i.name]
-                window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')
-            } else {
-                i.value = ''
-                i.nextElementSibling.removeAttribute('built')
-            }
-        })
-    }
-}
 window.LiveElement.Live.processors.IdeChannelTest = function(input) {
     var handlerType = window.LiveElement.Live.getHandlerType(input)
     if (handlerType == 'trigger') {
         var event = input.vector.split(':').shift()
         if (event == 'change') {
-            if (!window.LiveElement.Scale.Console.IDE.Channel.Test.socket 
-                || (window.LiveElement.Scale.Console.IDE.Channel.Test.socket && (window.LiveElement.Scale.Console.IDE.Channel.Test.socket.url != window.LiveElement.Scale.Console.IDE.Channel.Code.receive_url))) {
-                try {
-                    window.LiveElement.Scale.Console.IDE.Channel.Test.socket = new WebSocket(window.LiveElement.Scale.Console.IDE.Channel.Code.receive_url)
-                    window.LiveElement.Live.listen(window.LiveElement.Scale.Console.IDE.Channel.Test.socket, 'IdeChannelTest', 'message', false, true)
-                } catch(e) {
-                    delete window.LiveElement.Scale.Console.IDE.Channel.Test.socket
+            if (window.LiveElement.Scale.Console.IDE.Channel.channelElement 
+                && window.LiveElement.Scale.Console.IDE.Channel.channelElement['@id']
+                && window.LiveElement.Scale.Console.IDE.Channel.channelElement.receiveKey
+                && window.LiveElement.Scale.Console.IDE.Channel.channelElement.sendKey) {
+                var receiveUrl = `${window.LiveElement.Scale.Console.IDE.systemURL.replace('https:', 'wss:')}/channel/${window.LiveElement.Scale.Console.IDE.Channel.channelElement['@id']}/${window.LiveElement.Scale.Console.IDE.Channel.channelElement.receiveKey}`
+                if (!window.LiveElement.Scale.Console.IDE.Channel.Test.socket 
+                    || (window.LiveElement.Scale.Console.IDE.Channel.Test.socket && (window.LiveElement.Scale.Console.IDE.Channel.Test.socket.url != receiveUrl))) {
+                    try {
+                        window.LiveElement.Scale.Console.IDE.Channel.Test.socket = new WebSocket(receiveUrl)
+                        window.LiveElement.Live.listen(window.LiveElement.Scale.Console.IDE.Channel.Test.socket, 'IdeChannelTest', 'message', false, true)
+                    } catch(e) {
+                        delete window.LiveElement.Scale.Console.IDE.Channel.Test.socket
+                    }
                 }
+                var sendUrl = `${window.LiveElement.Scale.Console.IDE.systemURL}/channel/${window.LiveElement.Scale.Console.IDE.Channel.channelElement['@id']}/${window.LiveElement.Scale.Console.IDE.Channel.channelElement.sendKey}`
+                window.fetch(
+                    sendUrl, 
+                    {
+                        method: 'POST', 
+                        body: JSON.stringify(input.properties.value)
+                    }
+                ).then(() => {
+                    window.setTimeout(function() {
+                        input.triggersource.value = ''
+                    }, 250)
+                })
             }
-            window.fetch(
-                window.LiveElement.Scale.Console.IDE.Channel.Code.send_url, 
-                {
-                    method: 'POST', 
-                    body: JSON.stringify(input.properties.value)
-                }
-            ).then(() => {
-                window.setTimeout(function() {
-                    input.triggersource.value = ''
-                }, 250)
-            })
         }
     } else if (handlerType == 'subscription') {
         return {'#value': `${input.payload.message}\n${input.subscriber.value}`}
@@ -237,8 +118,7 @@ window.LiveElement.Live.processors.IdeChannelTest = function(input) {
     }
 }
 
-window.LiveElement.Live.listeners.IdeChannelSearch = {processor: 'IdeChannelSearch', expired: true}
-window.LiveElement.Live.listeners.IdeChannelTest = {processor: 'IdeChannelTest', expired: true}
+window.LiveElement.Scale.Console.buildSnippets('ide', 'channel')
 
-window.LiveElement.Live.listen(window.LiveElement.Scale.Console.IDE.pageElement.querySelector('section[name="channel"] fieldset[name="search"]'), 'IdeChannelSearch', 'loaded', false, true)
-*/
+window.LiveElement.Live.listeners.IdeChannelEdit = {processor: 'IdeChannelEdit', expired: true}
+window.LiveElement.Live.listeners.IdeChannelTest = {processor: 'IdeChannelTest', expired: true}
