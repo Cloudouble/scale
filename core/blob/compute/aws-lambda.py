d60b2cb13ee1a174693028941ab21c573350e1ab
David Pitman <david.pitman@cloudouble.com>
2021-06-14 16:44:17 +0000

    refactoring everything

---
+++ core/blob/compute/aws-lambda.py
@@ -0,0 +1,10 @@
+import env
+
+
+def run_sync(processor, *args):
+    pass
+
+def run_async(processor, *args):
+    pass
+
+
---
+++ core/blob/store/aws-s3.py
@@ -0,0 +1,21 @@
+import env
+
+
+def create(path, live=None, path=None, body=None, content_type=None, encoding=None):
+    pass
+
+def read(path):
+    return {
+        'path': path, 
+        'body': body, 
+        'content_type': content_type, 
+        'encoding': encoding, 
+        'live': live
+    }
+
+def update(path, live=None, path=None, body=None, content_type=None, encoding=None):
+    pass
+
+def delete(path):
+    pass
+
