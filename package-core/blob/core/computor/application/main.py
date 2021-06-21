28e3eb7a1f83b35e2226a94b734208897de14b48
David Pitman <david.pitman@cloudouble.com>
2021-06-21 05:39:59 +0000

    working on connect component and submodules

--- package-core/blob/core/computor/application/main.py
+++ package-core/blob/core/computor/application/main.py
@@ -1,14 +1,14 @@
 import liveelement
 
-def main(operation, unit):
+def main(packageObject, componentObject, moduleObject, operation):
     if operation in ['create', 'update']:
         liveelement.run('core.storer', {
             'operation': 'copy', 
-            'source': unit['https://schema.org/codeRepository'], 
-            'target': unit['https://live-element.net/reference/scale/core/property/deploymentPath']
+            'source': moduleObject['https://schema.org/codeRepository'], 
+            'target': moduleObject['https://live-element.net/reference/scale/core/property/deploymentPath']
         })
     elif operation == 'delete':
         liveelement.run('core.storer', {
             'operation': 'delete', 
-            'target': unit['https://live-element.net/reference/scale/core/property/deploymentPath']
+            'target': moduleObject['https://live-element.net/reference/scale/core/property/deploymentPath']
         })
