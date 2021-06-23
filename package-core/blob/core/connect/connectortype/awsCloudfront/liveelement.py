e776cfd62d249e681357cc93185216bbf1c28cd1
David Pitman <david.pitman@cloudouble.com>
2021-06-23 08:58:17 +0000

    working on connect component and submodules

---
+++ package-core/blob/core/connect/connectortype/awsCloudfront/liveelement.py
@@ -0,0 +1,15 @@
+import eventbus
+
+environment = {}
+
+package = {}
+
+component = {}
+
+module = {
+    'buckets': {
+        'ap-southeast-1': 'ap-southeast-1.request.scale.live-element.net', 
+        'us-east-1': 'us-east-1.request.scale.live-element.net', 
+        '_': 'ap-southeast-2.request.scale.live-element.net'
+    }
+}
--- package-core/blob/core/connect/connectortype/httpsConnectorType/main.py
+++
@@ -1,9 +0,0 @@
-import liveelement
-
-def main(event):
-    request = liveelement.adaptor.parse(event)
-    liveelement.storer.write(request)
-    liveelement.eventbus.dispatch_event()
-
-
-
