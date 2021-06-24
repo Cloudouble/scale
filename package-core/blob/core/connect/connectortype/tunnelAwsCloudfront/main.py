62a5a32b5fda0730b04f4c2eee944f463b59ab97
David Pitman <david.pitman@cloudouble.com>
2021-06-24 06:53:23 +0000

    working on connect component and submodules

--- package-core/blob/core/connect/connectortype/tunnelAwsCloudfront/main.py
+++ package-core/blob/core/connect/connectortype/tunnelAwsCloudfront/main.py
@@ -6,20 +6,20 @@ def main(packageObject, componentObject, moduleObject, operation):
         'operation': 'read', 
         'source': moduleObject['https://live-element.net/reference/scale/core/property/associatedAdaptorConfiguration']
     }, 'sync') if operation in ['create', 'update', 'delete'] else {}
-    if operation in ['create', 'update'] and adaptorConfiguration and adaptorConfiguration['DistributionConfig']:
+    if operation in ['create', 'update'] and adaptorConfiguration and adaptorConfiguration.get('CloudFront') and adaptorConfiguration['CloudFront'].get('DistributionConfig'):
         cloudfront_client = boto3.client('cloudfront')
-        if operation == 'update' and adaptorConfiguration['Id']:
-            cloudfront_client.update_distribution(DistributionConfig=adaptorConfiguration['DistributionConfig'], Id=adaptorConfiguration['Id'])
-        elif operation == 'create' and not adaptorConfiguration['Id']:
-            adaptorConfiguration['Id'] = cloudfront_client.create_distribution(DistributionConfig=adaptorConfiguration['DistributionConfig'])['Distribution']['Id']
+        if operation == 'update' and adaptorConfiguration['CloudFront'].get('Id'):
+            cloudfront_client.update_distribution(DistributionConfig=adaptorConfiguration['CloudFront']['DistributionConfig'], Id=adaptorConfiguration['CloudFront']['Id'])
+        elif operation == 'create' and not adaptorConfiguration['CloudFront'].get('Id'):
+            adaptorConfiguration['Cloudront']['Id'] = cloudfront_client.create_distribution(DistributionConfig=adaptorConfiguration['CloudFront']['DistributionConfig'])['Distribution']['Id']
             liveelement.run('core.storer', {
                 'operation': 'update', 
                 'source': adaptorConfiguration, 
                 'target': moduleObject['https://live-element.net/reference/scale/core/property/associatedAdaptorConfiguration'], 
             }, False, 'silent')
-    elif operation == 'delete' and adaptorConfiguration['Id']:
+    elif operation == 'delete' and adaptorConfiguration.get('CloudFront', {}).get('Id'):
         cloudfront_client.delete_distribution(Id=adaptorConfiguration['Id'])
-        del adaptorConfiguration['Id']
+        del adaptorConfiguration['CloudFront']['Id']
         liveelement.run('core.storer', {
             'operation': 'update', 
             'source': adaptorConfiguration, 
