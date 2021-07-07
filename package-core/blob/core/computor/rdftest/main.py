28812fda4433da6d673e156541529957a873c712
David Pitman <david.pitman@cloudouble.com>
2021-07-07 10:02:18 +0000

    working on processors and standardisation across the system

--- package-core/blob/core/computor/rdftest/main.py
+++ package-core/blob/core/computor/rdftest/main.py
@@ -14,6 +14,8 @@ def main(event, context):
     
     context_path = '../../../../system/core/value/context/core.json'
     
+    validator_path = '../../../../system/core/validator/class/Application.sh.json'
+    
     print(' ')
 
     with open(context_path) as context_file:
@@ -26,16 +28,37 @@ def main(event, context):
         
     object_object['@context'] = context_object
     
+    with open(validator_path) as validator_file:
+        validator_text = validator_file.read()
+    validator_object = json.loads(validator_text)
     
-        
         
     object_as_rdf_graph = Graph().parse(data=json.dumps(object_object), format='json-ld')
-    
-    #print(object_as_rdf_graph.serialize(format='n3', indent=4).decode('utf-8'))
 
-    #r = pyshacl.validate(object_as_rdf_graph, shacl_graph=validator_as_shacl_graph)
-    #conforms, results_graph, results_text = r    
+    validator_as_rdf_graph = Graph().parse(data=json.dumps(validator_object), format='json-ld')
+
     
+    print(object_as_rdf_graph.serialize(format='n3', indent=4).decode('utf-8'))
+    
+    print(' --- ')
+    
+    print(validator_as_rdf_graph.serialize(format='n3', indent=4).decode('utf-8'))
+
+    print(' --- ')
+    
+    r = pyshacl.validate(object_as_rdf_graph, shacl_graph=validator_as_rdf_graph)
+    
+    conforms, results_graph, results_text = r    
+    
+    print(' --- ')
+    print(conforms)
+    
+    print(' --- ')
+    print(results_graph.serialize(format='n3', indent=4).decode('utf-8'))
+
+    print(' --- ')
+    print(results_text)
+
     print(' ')
     
     return True
