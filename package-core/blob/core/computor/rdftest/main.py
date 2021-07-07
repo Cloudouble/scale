754c4caa48aff1c4aa75f4e521dde170d6a7f58a
David Pitman <david.pitman@cloudouble.com>
2021-07-07 09:08:52 +0000

    working on processors and standardisation across the system

--- package-core/blob/core/computor/rdftest/main.py
+++ package-core/blob/core/computor/rdftest/main.py
@@ -2,10 +2,33 @@ import json
 
 import pyshacl
 
+from rdflib import Graph, plugin
+from rdflib.serializer import Serializer
+
 
 def main(event, context):
+    
+    from os import listdir
+    
+    object_path = '../../../../system/core/application/console.json'
+    
+    print('starting...')
+    
+    with open(object_path) as object_file:
+        object_text = object_file.read()
+    object_object = json.loads(object_text)
+        
+        
+        
+    g = Graph().parse(data=object_text, format='json-ld')
+    
+    print(g.serialize(format='json-ld', indent=4))
+
+    #object_as_rdf_graph
     
     #r = pyshacl.validate(object_as_rdf_graph, shacl_graph=validator_as_shacl_graph)
     #conforms, results_graph, results_text = r    
     
-    return true
+    return True
+
+main({}, {})
