e4e75b8c7808f28aa147629239da618533c0b451
David Pitman <david.pitman@cloudouble.com>
2021-07-08 08:57:17 +0000

    working on processors and standardisation across the system

--- package-core/blob/core/platform/liveelement.py
+++ package-core/blob/core/platform/liveelement.py
@@ -153,6 +153,65 @@ def get_object(path, component=None, package='core', use_partition='system'):
     }
 }
 
+def set_object(path, data, encoding=None, content_type='application/json', component=None, package='core', use_partition='system'):
+    if data and type(data) is dict and 'Body' in data and type(data['Body']) is str:
+        partition = configuration['storer'].get(use_partition)
+        if partition and partition.get('driver'):
+            try:
+                driver = importlib.import_module('./drivers/{}'.format(partition['driver']))
+            except:
+                driver = None
+            if driver:
+                if component:
+                    if use_partition != 'system':
+                        system_partition = configuration['storer'].get('system')
+                        if system_partition and system_partition.get('driver'):
+                            system_partition_configuration = system_partition.get('configuration')
+                            try:
+                                system_partition_driver = importlib.import_module('./drivers/{}'.format(system_partition['driver']))
+                            except:
+                                system_partition_driver = None
+                    else:
+                        system_partition_driver = driver
+                    if system_partition_driver:
+                        try:
+                            component_object = json.loads( base64.b64decode(system_partition_driver.read(
+                                '{}/component/{}.json'.format(package.lower(), component.lower()), system_partition_configuration
+                                )['Body']).decode('utf-8'))
+                        except:
+                            component_object = {}
+                else:
+                    component_object = {}
+                if '.' not in path:
+                    filename_extension = component_object.get('defaultModuleFilenameExtension', 'json')
+                    if type(filename_extension) is dict:
+                        for subdir, ext in filename_extension.items():
+                            if path.startswith('{}/'.format(subdir)):
+                                filename_extension = ext
+                                break
+                    path = '{}.{}'.format(path, filename_extension)
+                if component:
+                    path = '{}/{}'.format(component.lower(), path)
+                if package:
+                    path = '{}/{}'.format(package.lower(), path)
+                    
+                if path.endswith('.json'):
+                    try:
+                        if type(data) is str:
+                            try:
+                                json.dumps(json.loads(data), indent=4, sort_keys=True)
+                            except:
+                                data = json.dumps(data, indent=4, sort_keys=True)
+                        else:
+                            data = json.dumps(data, indent=4, sort_keys=True)
+                    except:
+                        data = None
+                if type(data) is str:
+                    data = base64.b64encode(bytes(data, 'utf-8')).decode('utf-8')
+                    object_data = base64.b64decode(driver.read(path, partition.get('configuration'))['Body']).decode('utf-8')
+            
+
+
 
 def set_object(path, data, content_type='application/json', partition='system', component=None, package='core'):
     working_partitions = configuration.get('working_partitions', {})
