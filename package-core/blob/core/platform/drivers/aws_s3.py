106339ac12004728d160f5b3c50b3a38f927d60b
David Pitman <david.pitman@cloudouble.com>
2021-07-08 09:42:13 +0000

    working on processors and standardisation across the system

--- package-core/blob/core/platform/drivers/aws_s3.py
+++ package-core/blob/core/platform/drivers/aws_s3.py
@@ -2,21 +2,46 @@ import boto3, json, base64
 
 
 def read(path, configuration):
-    s3_object = boto3.client('s3').get_object(Bucket=configuration['Bucket'], Key='{}{}'.format(configuration.get('root', ''), path))
-    return {
-        'Body': base64.b64encode(s3_object['Body'].read()).decode('utf-8'), 
-        'ContentType': s3_object['ContentType'], 
-        'ContentLength': s3_object['ContentLength']
-    }
+    try:
+        s3_object = boto3.client('s3').get_object(**configuration.get('default_parameters', {}).get('read', {}), 
+            Bucket=configuration['Bucket'], Key='{}{}'.format(configuration.get('root', ''), path))
+        return {
+            'Body': base64.b64encode(s3_object['Body'].read()).decode('utf-8'), 
+            'ContentType': s3_object['ContentType'], 
+            'ContentLength': s3_object['ContentLength']
+        }
+    except:
+        return None
 
 
 def write(path, data_object, configuration):
-    pass
+    s3_arguments = {**configuration.get('default_parameters', {}).get('write', {}), 
+        'Bucket': configuration['Bucket'], 
+        'Key': '{}{}'.format(configuration.get('root', ''), path), 
+        'Body': base64.b64decode(data_object['Body'])
+    }
+    if data_object['ContentType']:
+        s3_arguments['ContentType'] = data_object['ContentType']
+    try:
+        boto3.client('s3').put_object(**s3_arguments)
+        return True
+    except:
+        return False
 
 
 def delete(path, configuration):
-    pass
+    try:
+        boto3.client('s3').delete_object(**configuration.get('default_parameters', {}).get('delete', {}), 
+            Bucket=configuration['Bucket'], Key='{}{}'.format(configuration.get('root', ''), path))
+        return True
+    except:
+        return False
 
 def ls(path, configuration):
-    pass
+    try:
+        list_result = boto3.client('s3').list_objects_v2(**configuration.get('default_parameters', {}).get('ls', {}), 
+            Bucket=configuration['Bucket'], Prefix='{}{}'.format(configuration.get('root', ''), path), Delimiter='/')
+        return sorted([c['Key'] for c in list_result['Contents']]  + [p['Prefix'] for p in list_result['CommonPrefixes']])
+    except:
+        return []
 
