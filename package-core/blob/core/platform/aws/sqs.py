eee600f72898173cf1b5b94188858aea639bb3d6
David Pitman <david.pitman@cloudouble.com>
2021-07-11 02:33:54 +0000

    working on processors and standardisation across the system

--- package-core/blob/core/platform/aws/sqs.py
+++ package-core/blob/core/platform/aws/sqs.py
@@ -6,26 +6,41 @@ def deploy_queue(queue_name, options={}, configuration={}):
         sqs_client = boto3.client('sqs')
         full_queue_name = '{namespace}-{queue_name}'.format(namespace=configuration['namespace'], queue_name=queue_name)
         try:
-            get_queue_result = sqs_client.get_queue(QueueName=full_queue_name)
+            get_queue_result = sqs_client.get_queue_url(QueueName=full_queue_name)
         except:
             get_queue_result = None
-        if get_queue_result:
+        if get_queue_result and get_queue_result.get('QueueUrl'):
             try:
-                if options and configuration.get('QueueUrl'):
-                    lambda_client.set_queue_attributes(QueueUrl=configuration['QueueUrl'], **options)
+                if options:
+                    sqs_client.set_queue_attributes(QueueUrl=get_queue_result['QueueUrl'], **options)
                 return True
             except:
                 return False
         else:
             try:
-                return sqs_client.create_queue(**configuration.get('default_parameters', {}).get('deploy_queue', {}), 
+                sqs_client.create_queue(**configuration.get('default_parameters', {}).get('deploy_queue', {}), 
                     QueueName=full_queue_name, **options)
+                return True
             except:
                 return False
     else:
         return None
 
-
+def remove_queue(queue_name, configuration={}):
+    if queue_name:
+        sqs_client = boto3.client('sqs')
+        full_queue_name = '{namespace}-{queue_name}'.format(namespace=configuration['namespace'], queue_name=queue_name)
+        try:
+            get_queue_result = sqs_client.get_queue_url(QueueName=full_queue_name)
+        except:
+            get_queue_result = None
+        if get_queue_result and get_queue_result.get('QueueUrl'):
+            try:
+                sqs_client.delete_queue(QueueUrl=get_queue_result['QueueUrl'])
+            except:
+                return False
+    else:
+        return None
 
 def send_message(message, configuration={}):
     if configuration and configuration.get('QueueUrl'):
